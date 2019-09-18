dc42b94ac17b78c4c6b501d9b9e5d3da77beee2c
Sumanth Ramanujapuram <sumra@tdc.dk>
2019-09-18 14:51:32 +0200

    Setup AB test/Feature toggle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  createInstance,
+  OptimizelyProvider,
+  OptimizelyFeature,
+} from '@optimizely/react-sdk'
+
+const optimizely = createInstance({
+  sdkKey: '2mc5jT4WaaNqNp44W5Hr14',
+})
+
+class App extends React.Component {
+  render() {
+    return (
+      <OptimizelyProvider
+        optimizely={optimizely}
+        user={{
+          id: 'user123',
+        }}
+      >
+        <div className='App'>
+          <h1>This is an AB test app</h1>
+          <OptimizelyFeature feature='feature_rollout_test'>
+            {(isEnabled) => (
+              isEnabled ?
+                <button className='primary'>Primary Button</button> :
+                <button className='secondary'>Secondary Button</button>
+            )}
+          </OptimizelyFeature>
+        </div>
+      </OptimizelyProvider>
+    )
+  }
 }
 
 export default App;
